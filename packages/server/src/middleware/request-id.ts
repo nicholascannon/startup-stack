95e45f294cfa9dab7fd1785bf6b4ed9b7d2f33e2
nicholascannon <nicholascannon1@gmail.com>
2026-01-04 16:03:28 +0800

    Remove source from req id middleware

--- packages/server/src/middleware/request-id.ts
+++ packages/server/src/middleware/request-id.ts
@@ -2,27 +2,15 @@ import { randomUUID } from 'node:crypto';
 import type { NextFunction, Request, Response } from 'express';
 
 declare global {
-	namespace Express {
-		interface Request {
-			requestId: string;
-			source: string | undefined;
-		}
-	}
+  namespace Express {
+    interface Request {
+      requestId: string;
+    }
+  }
 }
 
-export function requestIdMiddleware(
-	req: Request,
-	_res: Response,
-	next: NextFunction,
-) {
-	req.requestId =
-		typeof req.headers['x-request-id'] === 'string'
-			? req.headers['x-request-id']
-			: randomUUID();
+export function requestIdMiddleware(req: Request, _res: Response, next: NextFunction) {
+  req.requestId = typeof req.headers['x-request-id'] === 'string' ? req.headers['x-request-id'] : randomUUID();
 
-	req.source =
-		typeof req.headers['x-source'] === 'string'
-			? req.headers['x-source']
-			: undefined;
-	next();
+  next();
 }
