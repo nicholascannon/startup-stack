7489ffeb3db802407a1ec031576fe0508210ae9e
nicholascannon <nicholascannon1@gmail.com>
2026-01-30 17:17:19 +1100

    Fix SSL for DB migrations

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const certificate = process.env.DATABASE_CERTIFICATE
+  ? Buffer.from(process.env.DATABASE_CERTIFICATE, 'base64').toString('utf-8')
+  : undefined;
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
@@ -11,5 +15,6 @@ export default defineConfig({
   dbCredentials: {
     // biome-ignore lint/style/noNonNullAssertion: database URL is required
     url: process.env.DATABASE_URL!,
+    ssl: certificate ? { rejectUnauthorized: true, ca: certificate } : false,
   },
 });
