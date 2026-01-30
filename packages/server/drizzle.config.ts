4c20036d188eb440b6d4f64360c78d5a090eb9c5
nicholascannon <nicholascannon1@gmail.com>
2026-01-30 17:47:34 +1100

    Try something else for db migrations

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-const ca = process.env.DATABASE_CERTIFICATE
-  ? Buffer.from(process.env.DATABASE_CERTIFICATE, 'base64').toString('utf-8')
-  : undefined;
-
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
@@ -14,7 +10,6 @@ export default defineConfig({
   },
   dbCredentials: {
     // biome-ignore lint/style/noNonNullAssertion: database URL is required
-    url: process.env.DATABASE_URL!,
-    ssl: ca ? { rejectUnauthorized: true, ca } : undefined,
+    url: `${process.env.DATABASE_URL!}?sslmode=no-verify`,
   },
 });
